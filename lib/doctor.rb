4cba0ad85247361d73ce5f4a92ab7133352ee43d
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-22 08:38:06 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end
 
   def patients
-    appointments.collect {|appointment| appointment.patient}
+    appointments.map {|appointment| appointment.patient}
   end
 
   def self.all
